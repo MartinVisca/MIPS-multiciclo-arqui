6441a84837503319ff4b8d8c2840c80e03753789
Martin Visca <martinivisca@gmail.com>
2019-05-29 19:28:00 -0300

    Banco de registros
    Se agrego el banco de registros.

--- Procesador MIPS Multiciclo/Procesador MIPS Multiciclo.srcs/sources_1/new/P1D.vhd
+++ Procesador MIPS Multiciclo/Procesador MIPS Multiciclo.srcs/sources_1/new/P1D.vhd
@@ -1,6 +1,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -26,13 +28,31 @@ entity Registers is
 end Registers;
 
 architecture Behavioral of Registers is
-    signal d1 : std_logic_vector (31 downto 0);
-    signal d2 : std_logic_vector (31 downto 0);
-    signal dwr : std_logic_vector (31 downto 0);
+    type t_registro is array(0 to 31) of std_logic_vector (31 downto 0);
+    signal Regs: t_registro;
 begin
-    process(wr, clk)
+    process(wr, clk, reset, reg1_rd, reg2_rd, reg_wr)
     begin
-        if falling_edge(clk) and wr = '1' then
+        if (reset = '1') then --reset asincrono
+            Regs <= (others => x"00000000");    
+        else
+            if (falling_edge(clk)) then
+                if (wr = '1') then
+                    Regs(conv_integer(reg_wr)) <= data_wr;
+                end if;
+            else
+                if (reg1_rd /= "0000") then
+                    data1_rd <= Regs(conv_integer(reg1_rd));
+                else
+                    data1_rd <= x"00000000";
+                end if;
+                if (reg2_rd /= "0000") then
+                    data2_rd <= Regs(conv_integer(reg2_rd));
+                else
+                    data2_rd <= x"00000000";
+                end if;
+            end if;
+        end if;          
             
     end process;
 
